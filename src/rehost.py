dcbb1820cb16770ed3a7ebb2b438597d7d37d9eb
Abdi Abdalla <amabdalla10@gmail.com>
2015-07-04 02:42:24 -0500

    Changed rehost protocol. Now using Imgrush. Added video rehosting feature.

--- src/rehost.py
+++ src/rehost.py
@@ -2,14 +2,15 @@ import requests
 import json
 import credentials
 
-def rehost_img(image_url):
-        CLIENT_ID = credentials.credentials["IMGUR_CLIENT_ID"]
-        headers = {"Authorization": "Client-ID %s" % CLIENT_ID}
-        url = 'https://api.imgur.com/3/upload.json'
-        r2 = requests.post(
-                url,
-                headers = headers,
-                data={'key' : CLIENT_ID,
-                     'image': image_url})
-        j = json.loads(r2.text)
-        return str(j["data"]["link"])
+def rehost(media_url):
+    r2 = r.post('https://imgrush.com/api/upload/url', data={'url':media_url})
+    j = json.loads(r2.text)
+    hashcode = j['hash'].encode('utf-8')
+    p_info = json.loads((r.get(url='https://imgrush.com/api/%s' % hashcode)).text)
+
+    if p_info['blob_type'] == "image":
+        return p_info['files'][0]['url'].encode('utf-8')
+    elif p_info['blob_type'] == "video":
+        return p_info['files'][1]['url'].encode('utf-8')
+    else:
+        return "error"
