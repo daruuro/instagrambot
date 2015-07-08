b7d3864aa905620804021d8c395868eeffa760e7
Abdi Abdalla <amabdalla10@gmail.com>
2015-07-07 19:22:35 -0500

    Reworked the main bot method, now uniform way to deal with images or videos.

--- src/instagram_bot.py
+++ src/instagram_bot.py
@@ -12,44 +12,11 @@ import rehost
 import escape
 import sys
 import logging
+import comments
 from instagram.client import InstagramAPI
 from instagram.bind import InstagramAPIError
 
 logging.basicConfig(filename="logfile.log", format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
-
-image_comment = """
-[%s (@%s)](%s) posted this [Image](%s) at %s:
-> %s
->
-> **[Image](%s)**
->
-> **[Re-hosted](%s)**
-
----
-I'm just a bot that rehosts Instagram posts to Imgur. Don't have video working yet. Will soon!
-
-[^[Subreddit]](http://www.reddit.com/r/Instagram_Bot)
-[^[Code]](https://github.com/yaabdalla)
-[^[Creator]](http://www.reddit.com/u/drogbafan)
-"""
-
-video_comment = """
-[%s (@%s)](%s) posted this [Video](%s) at %s:
-> %s
->
-> **[Thumbnail](%s)**
->
-> **[Video](%s)**
->
-> **%s**
-
----
-I'm just a bot that rehosts Instagram posts to Imgur. Can't rehost videos just yet, working on it!
-
-[^[Subreddit]](http://www.reddit.com/r/Instagram_Bot)
-[^[Code]](https://github.com/yaabdalla)
-[^[Creator]](http://www.reddit.com/u/drogbafan)
-"""
 
 db = sqlite3.connect('igdb.db')
 curs = db.cursor()
@@ -78,61 +45,32 @@ while True:
                 media_id = get_media_id.get_media_id(str(submission.url)).encode('utf-8').strip()
                 this_media = api.media(media_id)
 
-                if this_media.type == "image":
-                    author_full_name = this_media.user.full_name.encode('utf-8')
-                    author_name = this_media.user.username.encode('utf-8')
-                    author_url = "http://www.instagram.com/%s" % author_name
-                    post_url = this_media.link.encode('utf-8')
-                    post_date = this_media.created_time.strftime("%Y-%m-%d %H:%M:%S")
-                    try:
-                        caption = escape.escape_chars(this_media.caption.text.encode('utf-8'))
-                    except:
-                        caption = ""
-                    image_url = this_media.images['standard_resolution'].url.encode('utf-8')
-                    rehosted_url = (rehost.rehost_img(this_media.images['standard_resolution'].url)).encode('utf-8')
-                    try:
-                        curs.execute("INSERT INTO reddit (ID, TIMESTAMP) VALUES (?, CURRENT_TIMESTAMP)",(str(submission.id),))
-                        logging.debug("Succesfully added submission " + str(submission.id) + " to the database")
-                        db.commit()
-                        logging.debug("Successfully committed submission " + str(submission.id) + " to the database")
-                    except sqlite3.Error as e:
-                        logging.critical("Unable to add submission: " + str(submission.id) + " to the database because of : " + str(e))
-                        continue
-                    try:
-                        submission.add_comment(image_comment % (author_full_name, author_name, author_url, post_url, post_date, caption, image_url, rehosted_url))
-                        logging.debug("Successfully submitted comment for submission: " + str(submission.id))
-                    except praw.errors.APIException as e:
-                        logging.critical("Comment submission for "+ str(submission.id) + " not processed because " + str(e))
-                        continue
-                    logging.info("Succesfully processed and posted comment at " + "http://www.reddit.com/" + str(submission.id))
-                elif this_media.type == "video":
-                    author_full_name = this_media.user.full_name.encode('utf-8')
-                    author_name = this_media.user.username.encode('utf-8')
-                    author_url = "http://www.instagram.com/%s" % author_name
-                    post_url = this_media.link.encode('utf-8')
-                    post_date = this_media.created_time.strftime("%Y-%m-%d %H:%M:%S")
-                    try:
-                        caption = escape.escape_chars(this_media.caption.text.encode('utf-8'))
-                    except:
-                        caption = ""
-                    thumbnail_url = (this_media.images['standard_resolution'].url).encode('utf-8')
-                    video_url = (this_media.videos['standard_resolution'].url).encode('utf-8')
-                    rehosted_url = "Rehosted video feature not available right now. If you have suggestions, please let me know at /r/Instagram-Bot."
-                    try:
-                        curs.execute("INSERT INTO reddit (ID, TIMESTAMP) VALUES (?, CURRENT_TIMESTAMP)",(str(submission.id),))
-                        logging.debug("Succesfully added submission " + str(submission.id) + " to the database")
-                        db.commit()
-                        logging.debug("Succesfully committed submission " + str(submission.id) + " to the database")
-                    except sqlite3.Error as e:
-                        logging.critical("Unable to add submission: " + str(submission.id) + " to the database because of : " + str(e))
-                        continue
-                    try:
-                        submission.add_comment(video_comment % (author_full_name, author_name, author_url, post_url, post_date, caption, thumbnail_url, video_url, rehosted_url))
-                        logging.debug("Successfully submitted comment for submission: " + str(submission.id))
-                    except praw.errors.APIException as e:
-                        logging.critical("Comment submission for "+ str(submission.id) + " not processed because " + str(e))
-                        continue
-                    logging.info("Succesfully processed and posted comment at " + "http://www.reddit.com/" + str(submission.id))
+                media_type = this_media.type.encode('utf-8').capitalize()
+                formatted_comment = comments.comment.format(media_type)
+
+                author_full_name = this_media.user.full_name.encode('utf-8')
+                author_name = this_media.user.username.encode('utf-8')
+                author_url = "http://www.instagram.com/%s" % author_name
+                post_url = this_media.link.encode('utf-8')
+                post_date = this_media.created_time.strftime("%Y-%m-%d %H:%M:%S")
+                caption = escape.escape_chars(this_media.caption.text.encode('utf-8')) if this_media.caption.text != None else ""
+                media_url = this_media.images['standard_resolution'].url.encode('utf-8') if media_type == "Image" else this_media.videos['standard_resolution'].url.encode('utf-8')
+                rehosted_url = rehost.rehost(media_url)
+                try:
+                    curs.execute("INSERT INTO reddit (ID, TIMESTAMP) VALUES (?, CURRENT_TIMESTAMP)",(str(submission.id),))
+                    logging.debug("Succesfully added submission " + str(submission.id) + " to the database")
+                    db.commit()
+                    logging.debug("Successfully committed submission " + str(submission.id) + " to the database")
+                except sqlite3.Error as e:
+                    logging.critical("Unable to add submission: " + str(submission.id) + " to the database because of : " + str(e))
+                    continue
+                try:
+                    submission.add_comment(formatted_comment % (author_full_name, author_name, author_url, post_url, post_date, caption, media_url, rehosted_url))
+                    logging.debug("Successfully submitted comment for submission: " + str(submission.id))
+                except praw.errors.APIException as e:
+                    logging.critical("Comment submission for "+ str(submission.id) + " not processed because " + str(e))
+                    continue
+                logging.info("Succesfully processed and posted comment at " + "http://www.reddit.com/" + str(submission.id))
             except ValueError as e:
                 logging.critical("Not proccessed because of this error: " + str(e) + ". Line number: " + str(sys.exc_info()[-1].tb_lineno))
             except InstagramAPIError as e:
