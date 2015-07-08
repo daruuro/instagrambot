083853203585ea63fa32b1c36be820d7cc16e2cc
Abdi Abdalla <amabdalla10@gmail.com>
2015-07-07 19:01:09 -0500

    Added uniform comments, in seperate file. Less repetition.

---
+++ src/comments.py
@@ -0,0 +1,17 @@
+
+
+comment = """
+[%s (@%s)](%s) posted this [{0}](%s) at %s:
+> %s
+>
+> **[{0}](%s)**
+>
+> **[Re-hosted](%s)**
+
+---
+I'm just a bot that rehosts Instagram posts to Imgur. Don't have video working yet. Will soon!
+
+[^[Subreddit]](http://www.reddit.com/r/Instagram_Bot)
+[^[Code]](https://github.com/yaabdalla/instagrambot)
+[^[Creator]](http://www.reddit.com/u/drogbafan)
+"""
