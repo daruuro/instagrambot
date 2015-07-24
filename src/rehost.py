a550a0de65d16de7814423f0495b66fe9c2381ca
Abdi Abdalla <amabdalla10@gmail.com>
2015-07-24 18:10:51 -0500

    I don't even know what 's changed. It says it's changed so I believe it.

--- src/rehost.py
+++ src/rehost.py
@@ -3,16 +3,17 @@ import json
 import credentials
 
 def rehost(media_url):
-    r2 = r.post('https://imgrush.com/api/upload/url', data={'url':media_url})
-    j = json.loads(r2.text)
-    hashcode = j['hash'].encode('utf-8')
-    p_info = json.loads((r.get(url='https://imgrush.com/api/%s' % hashcode)).text)
+    try:
+        r2 = r.post('https://imgrush.com/api/upload/url', data={'url':media_url})
+        j = json.loads(r2.text)
+        hashcode = j['hash'].encode('utf-8')
+        p_info = json.loads((r.get(url='https://imgrush.com/api/%s' % hashcode)).text)
 
-    if p_info['blob_type'] == "image":
-        return p_info['files'][0]['url'].encode('utf-8')
-    elif p_info['blob_type'] == "video":
-        return p_info['files'][1]['url'].encode('utf-8')
-    else:
+        if p_info['blob_type'] == "image":
+            return p_info['files'][0]['url'].encode('utf-8')
+        elif p_info['blob_type'] == "video":
+            return p_info['files'][1]['url'].encode('utf-8')
+    except Exception as e:
         return "error"
 
 def delete_rehost(hashcode):
