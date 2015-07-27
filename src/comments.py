987c9d90168ad9748f6716e46f56ff9cebef8013
Abdi Abdalla <amabdalla10@gmail.com>
2015-07-26 22:30:25 -0500

    Added deletion messaging link to comment template.

--- src/comments.py
+++ src/comments.py
@@ -9,7 +9,9 @@ comment = """
 > **[Re-hosted](%s)**
 
 ---
-I'm just a bot that rehosts Instagram posts to Imgur. Don't have video working yet. Will soon!
+I'm just a bot that rehosts Instagram posts to Imgur.
+
+To delete this post, [click here](https://www.reddit.com/message/compose/?to=instagram-bot&subject=Deletion%%20Request&message=%%2Bdelete%%20__id__). Must be OP to delete post. If not OP, but want to delete-[click here](https://www.reddit.com/message/compose/?to=drogbafan&subject=Deletion%%20Request&message=Please%%20list%%20reason%%20for%%20deletion%%20request%%20below%%20(e.i.%%20Image%%20is%%20yours,%%20you%%20know%%20the%%20person%%20in%%20the%%20image,%%20etc.\):).
 
 [^[Subreddit]](http://www.reddit.com/r/Instagram_Bot)
 [^[Code]](https://github.com/yaabdalla/instagrambot)
